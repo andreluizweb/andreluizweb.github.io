3f07800156ef7fd21a2f4f6986dd6bc5a69b2067
André Luiz Batista <andre.batista@byintera.com>
2024-05-01 17:24:18 -0300

    Change to PWA

--- dieta/src/index.tsx
+++ dieta/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root, { loader as personLoader } from "./routes/root";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -27,7 +27,9 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorker.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
